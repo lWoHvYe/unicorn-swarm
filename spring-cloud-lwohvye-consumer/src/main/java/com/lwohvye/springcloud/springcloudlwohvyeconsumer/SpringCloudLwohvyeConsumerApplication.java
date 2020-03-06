e3ae316df6c30c4a9866d8c130f25356eb786666
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-03-06 18:39:31 +0800

    consumer层初版提交

---
+++ spring-cloud-lwohvye-consumer/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeconsumer/SpringCloudLwohvyeConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLwohvyeConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudLwohvyeConsumerApplication.class, args);
+    }
+
+}
