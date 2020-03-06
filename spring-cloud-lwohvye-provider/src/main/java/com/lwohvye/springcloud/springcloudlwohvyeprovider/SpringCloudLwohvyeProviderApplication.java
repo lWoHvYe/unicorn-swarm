a028eb4b46a56c5b72bb267068ea87541bcb7948
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-03-06 12:53:33 +0800

    provider层初版代码

---
+++ spring-cloud-lwohvye-provider/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeprovider/SpringCloudLwohvyeProviderApplication.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLwohvyeProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudLwohvyeProviderApplication.class, args);
+    }
+
+}
