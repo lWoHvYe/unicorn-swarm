2e3c76f1e03dacedf81e815375cf7c834b053283
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-03-06 12:31:02 +0800

    api层初版代码

---
+++ spring-cloud-lwohvye-api/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeapi/SpringCloudLwohvyeApiApplication.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLwohvyeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudLwohvyeApiApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-lwohvye-api/src/test/java/com/lwohvye/springcloud/springcloudlwohvyeapi/SpringCloudLwohvyeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLwohvyeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
