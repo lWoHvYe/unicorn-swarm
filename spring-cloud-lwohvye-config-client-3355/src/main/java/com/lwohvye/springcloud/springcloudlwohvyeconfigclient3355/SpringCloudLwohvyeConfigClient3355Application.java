0eaa1ecb8c60849ff13dee8dc7b499bf781c6b47
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-03-08 22:07:39 +0800

    Config-Client初版提交

---
+++ spring-cloud-lwohvye-config-client-3355/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeconfigclient3355/SpringCloudLwohvyeConfigClient3355Application.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeconfigclient3355;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLwohvyeConfigClient3355Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudLwohvyeConfigClient3355Application.class, args);
+    }
+
+}
---
+++ spring-cloud-lwohvye-config-client-3355/src/test/java/com/lwohvye/springcloud/springcloudlwohvyeconfigclient3355/SpringCloudLwohvyeConfigClient3355ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeconfigclient3355;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLwohvyeConfigClient3355ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
