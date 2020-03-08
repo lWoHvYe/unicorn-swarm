ca34096d37fdd7f4311f85ab278788d2e8b2cc06
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-03-08 17:44:37 +0800

    服务监控hystrix dashboard访问地址http://localhost:9001/hystrix

---
+++ spring-cloud-lwohvye-consumer-hystrix/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeconsumer/config/ConfigBean.java
@@ -0,0 +1,34 @@
+package com.lwohvye.springcloud.springcloudlwohvyeconsumer.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Hongyan Wang
+ * @packageName com.lwohvye.springcloud.springcloudlwohvyeconsumer.config
+ * @className ConfigBean
+ * @description
+ * @date 2020/3/8 16:46
+ */
+@Configuration
+public class ConfigBean {
+    /**
+     * 此配置是为了服务监控而配置，与服务容错本身无观，springCloud 升级之后的坑
+     * ServletRegistrationBean因为springboot的默认路径不是/hystrix.stream
+     * 只要在自己的项目中配置上下面的servlet即可
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+
+    }
+}
