638084112bd213137a49f5c66f261f37e4bdb40e
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2020-03-25 14:35:14 +0800

    Spring Security测试

---
+++ spring-cloud-lwohvye-consumer-security/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeconsumersecurity/SpringCloudLwohvyeConsumerSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeconsumersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLwohvyeConsumerSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLwohvyeConsumerSecurityApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-lwohvye-consumer-security/src/test/java/com/lwohvye/springcloud/springcloudlwohvyeconsumersecurity/SpringCloudLwohvyeConsumerSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwohvye.springcloud.springcloudlwohvyeconsumersecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLwohvyeConsumerSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
