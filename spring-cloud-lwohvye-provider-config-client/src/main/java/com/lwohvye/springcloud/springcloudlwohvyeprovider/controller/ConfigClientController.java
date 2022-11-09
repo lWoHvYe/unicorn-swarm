a6d10d168088ee371d887f92f3342795e31cb1bb
王红岩 lWoHvYe <lwohvye@outlook.com>
2022-11-09 08:59:52 +0800

    Client Read MultiConfigFile from ConfigServer

--- spring-cloud-lwohvye-provider-config-client/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeprovider/controller/ConfigClientController.java
+++ spring-cloud-lwohvye-provider-config-client/src/main/java/com/lwohvye/springcloud/springcloudlwohvyeprovider/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.lwohvye.springcloud.springcloudlwohvyeprovider.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RefreshScope
+// @PropertySource("classpath:cloud-lwohvye-extra-config.yml") 使用config server不要用这个，如果这个是从server获取的，会报fileNotFound，不加也能读到
 public class ConfigClientController {
 
     @Value("${config.info}")
