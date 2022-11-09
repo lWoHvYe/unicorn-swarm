3dff8e5fb7d612bd2848051a93dbf533c1483925
王红岩 lWoHvYe <lwohvye@outlook.com>
2022-11-09 19:40:11 +0800

    fixed UT error: No scope registered for scope name 'refresh'

--- spring-cloud-lwohvye-consumer-config-client/src/test/java/com/lwohvye/springcloud/springcloudlwohvyeconsumer/SpringCloudLwohvyeConsumerConfigApplicationTests.java
+++ spring-cloud-lwohvye-consumer-config-client/src/test/java/com/lwohvye/springcloud/springcloudlwohvyeconsumer/SpringCloudLwohvyeConsumerConfigApplicationTests.java
@@ -1,9 +1,12 @@
 package com.lwohvye.springcloud.springcloudlwohvyeconsumer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 
 @SpringBootTest
+@ImportAutoConfiguration(RefreshAutoConfiguration.class) // fixed UT error: No scope registered for scope name 'refresh'
 class SpringCloudLwohvyeConsumerConfigApplicationTests {
 
     @Test
--- spring-cloud-lwohvye-provider-config-client/src/test/java/springcloudlwohvye/springcloudlwohvyeproviderconfigclient/SpringCloudLwohvyeProviderConfigClientApplicationTests.java
+++ spring-cloud-lwohvye-provider-config-client/src/test/java/springcloudlwohvye/springcloudlwohvyeproviderconfigclient/SpringCloudLwohvyeProviderConfigClientApplicationTests.java
@@ -1,9 +1,12 @@
 package springcloudlwohvye.springcloudlwohvyeproviderconfigclient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 
 @SpringBootTest
+@ImportAutoConfiguration(RefreshAutoConfiguration.class)
 class SpringCloudLwohvyeProviderConfigClientApplicationTests {
 
     @Test
